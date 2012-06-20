892043dc07e2030df07fff993043b6f6194400c3
Holger Ihrig <holger.ihrig@gmail.com>
2012-06-20 18:53:25 +0200

    Fixed Writing of Settings

--- iBazar/settings.cpp
+++ iBazar/settings.cpp
@@ -1,30 +1,44 @@
-#include "settingsdialog.h"
+#include "settings.h"
+#include <QDebug>
 
-
-SettingsDialog::SettingsDialog(QSettings *settings, QWidget *parent) :
-    QDialog(parent),
-    ui(new Ui::SettingsDialog)
+Settings::Settings(QSettings *settings, QWidget *parent) :
+    QDialog(parent)
 {
     setupUi(this);
 
     dialogsettings = settings;
 
-    connect(ui->okButton,SIGNAL(clicked()),this,SLOT(saveAndQuit()));
-    connect(ui->abortButton,SIGNAL(clicked()),this,SLOT(close()));
+    readSettings();
+
+    connect(okButton,SIGNAL(clicked()),this,SLOT(save()));
+    connect(abortButton,SIGNAL(clicked()),this,SLOT(close()));
 }
 
-void SettingsDialog::saveAndQuit()
+void Settings::readSettings()
 {
-    if (!ui->inputhostname->text().isEmpty())
-        dialogsettings->setValue("DbHostname",ui->inputhostname->text());
-    if (!ui->inputdbname->text().isEmpty())
-        dialogsettings->setValue("DbName",ui->inputdbname->text());
-    if (!ui->inputuser->text().isEmpty())
-        dialogsettings->setValue("DbUser",ui->inputuser->text());
-    if (!ui->inputpasswd->text().isEmpty())
-        dialogsettings->setValue("DbPasswd",ui->inputpasswd->text());
+    if (!dialogsettings->value("DbHostname").isNull())
+        inputhostname->setText(dialogsettings->value("DbHostname").toString());
+    if (!dialogsettings->value("DbName").isNull())
+        inputdbname->setText(dialogsettings->value("DbName").toString());
+    if (!dialogsettings->value("DbUser").isNull())
+        inputuser->setText(dialogsettings->value("DbUser").toString());
+    if (!dialogsettings->value("DbPasswd").isNull())
+        inputpasswd->setText(dialogsettings->value("DbPasswd").toString());
+}
 
-    this->close();
+void Settings::save()
+{
+    if (!inputhostname->text().isEmpty())
+        dialogsettings->setValue("DbHostname",inputhostname->text());
+    if (!inputdbname->text().isEmpty())
+        dialogsettings->setValue("DbName",inputdbname->text());
+    if (!inputuser->text().isEmpty())
+        dialogsettings->setValue("DbUser",inputuser->text());
+    if (!inputpasswd->text().isEmpty())
+        dialogsettings->setValue("DbPasswd",inputpasswd->text());
+
+    close();
+
 }
 
 
--- iBazar/settings.h
+++ iBazar/settings.h
@@ -6,20 +6,22 @@
 
 #include "ui_settingsdialog.h"
 
-class SettingsDialog : public QDialog, public Ui::SettingsDialog
+class Settings : public QDialog, public Ui::SettingsDialog
 {
     Q_OBJECT
 public:
-    explicit SettingsDialog(QSettings *settings, QWidget *parent = 0);
+    explicit Settings(QSettings *settings, QWidget *parent = 0);
+
 
 private:
     QSettings *dialogsettings;
-    Ui::SettingsDialog *ui;
+
+    void readSettings();
     
 signals:
     
 public slots:
-    void saveAndQuit();
+    void save();
     
 };
 
